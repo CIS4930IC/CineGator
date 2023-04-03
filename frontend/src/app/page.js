084545f11c9f9a86e57b594d3c42d08791d0d97c
mikelxk <mike.lxk@gmail.com>
2023-04-03 15:46:15 -0400

    add login page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-
+import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -12,21 +12,11 @@ export default function Home() {
           CineGator
         </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <Link href="/login">
+            <p>
+              Sign in
+            </p>
+          </Link>
         </div>
       </div>
 
