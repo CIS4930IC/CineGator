09eb6c328858910a9bf7cb74e87633162ddf1ebd
Mike Pangas <mpangas@ufl.edu>
2023-04-27 18:34:45 -0400

    Home Page Changes

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -7,75 +7,28 @@ const inter = Inter({ subsets: ["latin"] })
 export default function Home() {
   return (
     <header>
-      <nav
-        className="fixed z-10 top-0 inset-x-0 flex w-full items-center justify-between py-2 text-neutral-600 shadow-lg focus:text-neutral-800 dark:text-neutral-200 md:flex-wrap md:justify-start"
-        data-te-navbar-ref
-      >
-
-        <div className="px-6">
-          <div
-            className="!visible hidden grow basis-[100%] items-center md:!flex md:basis-auto"
-            id="navbarSupportedContentO"
-            data-te-collapse-item
-          >
-            <ul
-              className="mr-auto flex flex-col md:flex-row items-center"
-              data-te-navbar-nav-ref
-            >
-              <li data-te-nav-item-ref>
-                <a
-                  className="block transition duration-150 ease-in-out md:p-2 [&.active]:border-primary [&.active]:text-primary mr-5 text-2xl"
-                  href="#!"
-                  data-te-nav-link-ref
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  CineGator
-                </a>
+      <nav class="bg-white dark:bg-black fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="/" class="flex items-center">
+            <Image src="/images/logo.png" alt="Flowbite Logo" height={1} width={190} />
+          </a>
+          <div class="flex md:order-2">
+            <button type="button" class="text-red-700 md:dark:text-red-500 hover:text-red-900 dark:hover:text-red-700 bg-transparent focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 ">Log In</button>
+            <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-red-600 dark:hover:bg-red-700">Sign Up</button>
+          </div>
+          <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:border-gray-700">
+              {/* <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-white bg-red-700 rounded md:bg-transparent md:text-red-700 md:p-0 md:dark:text-red-500">Home</a>
+              </li> */}
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:dark:hover:text-red-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Browse</a>
               </li>
-              <li data-te-nav-item-ref>
-                <a
-                  className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 dark:hover:text-white dark:focus:text-white md:p-2 [&.active]:border-primary [&.active]:text-primary"
-                  href="#!"
-                  data-te-nav-link-ref
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  Home
-                </a>
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:dark:hover:text-red-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
               </li>
-              <li data-te-nav-item-ref>
-                <a
-                  className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 dark:hover:text-white dark:focus:text-white md:p-2 [&.active]:border-primary [&.active]:text-primary"
-                  href="#!"
-                  data-te-nav-link-ref
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  Features
-                </a>
-              </li>
-              <li data-te-nav-item-ref>
-                <a
-                  className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 dark:hover:text-white dark:focus:text-white md:p-2 [&.active]:border-primary [&.active]:text-primary"
-                  href="#!"
-                  data-te-nav-link-ref
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  About
-                </a>
-              </li>
-              <li data-te-nav-item-ref>
-                <a
-                  className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 dark:hover:text-white dark:focus:text-white md:p-2 [&.active]:border-primary [&.active]:text-primary ml:5"
-                  href="#!"
-                  data-te-nav-link-ref
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  Sign in
-                </a>
+              <li>
+                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:dark:hover:text-red-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">GitHub</a>
               </li>
             </ul>
           </div>
