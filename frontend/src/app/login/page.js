d04812fa95a31b7e741c81ec28df46c9bad0bea1
mikelxk <mike.lxk@gmail.com>
2023-04-29 01:16:43 -0400

    fix: use htmlFor chore: up deps

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,7 +1,7 @@
 'use client'
-import Link from 'next/link'
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react'
+import { useState } from 'react';
 
 export default function Login() {
   const router = useRouter();
@@ -30,14 +30,14 @@ export default function Login() {
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" action="#" method="POST">
           <div>
-            <label for="username" class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Username</label>
+            <label htmlFor='username' class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Username</label>
             <div class="mt-2">
               <input onChange={(event) => setUsername(event.target.value)} id="username" name="username" type="username" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" />
             </div>
           </div>
           <div>
             <div class="flex items-center justify-between">
-              <label for="password" class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Password</label>
+              <label htmlFor="password" class="block text-sm font-medium leading-6 text-gray-900 dark:text-white">Password</label>
             </div>
             <div class="mt-2">
               <input onChange={(event) => setPassword(event.target.value)} id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" />
@@ -50,7 +50,7 @@ export default function Login() {
         </form>
 
         <p class="mt-10 text-center text-sm text-gray-500 dark:text-white">
-          Not a user? 
+          Not a user?
           <Link href='/signup' class="font-semibold leading-6 text-red-600 hover:text-red-800 dark:hover:text-red-800"> Sign up for an account.</Link>
         </p>
       </div>
