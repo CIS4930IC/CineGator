1ce7c2d67ecb5b8fc8d6b99404937d95274ed471
Mike Pangas <mpangas@ufl.edu>
2023-05-01 19:46:02 -0400

    Signup input validation

--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -8,15 +8,36 @@ export default function Signup() {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError("Invalid email address.")
+      return;
+    }
+
+    // Username validation
+    const usernameRegex = /^[a-zA-Z0-9_]{3,16}$/;
+    if (!usernameRegex.test(username)) {
+      setError("Username must be between 3 and 16 characters long and can only contain letters, numbers, and underscores.")
+      return;
+    }
+
+    // Password validation
+    const passwordRegex = /^[a-zA-Z0-9&%@$!]+$/;
+    if (password.length < 6 || !passwordRegex.test(password)) {
+      setError("Password must be at least 6 characters long and only contain letters, numbers, and &, %, @, $, or ! characters.");
+      return;
+    }
+
+    // If all inputs are valid, submit the form
     console.log("Email: " + email);
     console.log("Username: " + username);
     console.log("Password: " + password);
-
-    // To-Do - Add validation
 
     // To-Do - Add API call to create user
 
@@ -50,6 +71,9 @@ export default function Signup() {
             <div className="mt-2">
               <input onChange={(event) => setPassword(event.target.value)} id="password" name="password" type="password" autocomplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" />
             </div>
+          </div>
+          <div className="flex">
+            {error && <div className="text-red-600 text-sm">{error}</div>}
           </div>
 
           <div>
