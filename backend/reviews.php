78183c96f054a6161485464b8c3e81003804a4af
Mike Pangas <mpangas@ufl.edu>
2023-05-03 00:41:36 -0400

    Finished reviews.php

--- backend/reviews.php
+++ backend/reviews.php
@@ -1,25 +1,37 @@
-#!/usr/local/bin/php
-    
 <?php
-    //Initial config
-    $config = parse_ini_file("database/db_config.ini");
+$config = parse_ini_file("database/db_config.ini");
 
-    //Establishing connection
-    $connection = new mysqli($config["servername"], $config["username"], $config["password"], $config["dbname"]);
+$connection = mysqli_init();
+mysqli_real_connect($connection, $config["servername"], $config["username"], $config["password"], $config["dbname"], 3306, NULL, MYSQLI_CLIENT_SSL);
 
-    if ($connection->connect_error) {
-        die("Connection failed: " . $connection->connect_error);
-    }
+if ($connection->connect_error) {
+    $response = array("status" => "error", "message" => "Connection failed: " . $connection->connect_error);
+    echo json_encode($response);
+    exit();
+}
 
-    //HOW TO GET MOVIE_ID?
-    $sql = "SELECT * FROM reviews WHERE movie_id = ";
+// Get Movie ID and make query
+if (isset($_POST["movieID"])) {
+    $movie_id = trim($_POST["movieID"]);
+    $sql = "SELECT * FROM reviews WHERE movieID = $movie_id";
     $result = $connection->query($sql);
+    if ($result !== false && $result->num_rows > 0) {
+        // Return json array of reviews
+        while ($row = $result->fetch_assoc()) {
+            $reviews[] = $row;
+        }
 
-    while($row = $result->fetch_assoc()) {
-
-
-    //PRINT NAME OF USER FROM USER_ID?
+        http_response_code(200);
+        echo json_encode($reviews);
+    } else {
+        http_response_code(200);
+        echo json_encode(array("message" => "No reviews found."));
     }
+} else {
+    http_response_code(400);
+    echo json_encode(array("message" => "Provide movie ID."));
+}
 
-    $connection->close();
+exit();
+$connection->close();
 ?>
