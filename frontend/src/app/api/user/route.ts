5f915330440ef9353c89866a5d614dea247d2a4e
mikelxk <mike.lxk@gmail.com>
2023-05-02 21:56:28 -0400

    style: always use dark mode remove user api route

--- frontend/src/app/api/user/route.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-import { NextResponse } from "next/server"
-const isDev = true
-const prisma = new PrismaClient()
-
-export async function GET(request: Request) {
-  const users = isDev ? await prisma.users.findMany() : "not in Dev"
-  return NextResponse.json({ users })
-}
