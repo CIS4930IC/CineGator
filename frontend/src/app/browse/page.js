b8c31568c55bd1ef2024923751e465a85339f7f1
Mike Pangas <mpangas@ufl.edu>
2023-05-03 22:10:52 -0400

    Removed dupe frontend

--- frontend/src/app/browse/page.js
+++
@@ -1,9 +0,0 @@
-"use client"
-import useMovie from "../util/useMovie"
-
-export default function Browse() {
-    const { movie, isLoading, isError } = useMovie(502356)
-    return (
-        <div></div>
-    )
-}
